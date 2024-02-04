20a722eb4ae811ba643cf24fdafb8957d131f0f8
lms2004 <mxylms1210@163.com>
2024-02-03 19:18:11 -0800

    文案生成app,0.3版本发布

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da45c7d80e5347b289580816d5b668054de1a48a7f4104dff6f9ab3386ac979a
-size 14327
+oid sha256:e063fa77bcbb082cf4a769076324bc0ecc905942b661de863a211c8229739f6d
+size 14348
